978f1307e1feb83df755941ba8692120a7155f59
divejikan-yuvarajah <divejuthe@gmail.com>
2025-02-19 09:55:21 +0530

    New file created

---
+++ first.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main ()
+{
+  printf("Welcome to the Fundamentals Of Programming Session...!");
+  printf("Yuvarajah Divejikan - SEU/IS/22/ICT/067..");
+  printf("19-02-2025");
+  return 0;
+}
